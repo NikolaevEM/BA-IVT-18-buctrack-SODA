a14368e2ef08651c514869c484f69b5ab42c31ee
Mikhail Petrov <43157772+petrovmm@users.noreply.github.com>
2021-04-09 13:10:09 +0900

    Классы (context fixes)

---
+++ PE1/Bug.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace PE1
+{
+    public class Bug
+    {
+        [Key]
+        public int BugId { get; set; }
+        public int BugTypeId { get; set; }
+        public BugType BugType { get; set; }
+        public string Description { get; set; }
+        public DateTime LastFixedDate { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int StatusBugId { get; set; }
+        public StatusBug Status { get; set; }
+        public int DivisionId { get; set; }
+        public Division Division { get; set; }
+
+        public virtual ICollection<Fix> Fixes { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        
+
+    }
+}
