d5f0d290e1c047ee55ef5e44ba90c610d296fb38
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2021-05-14 12:45:34 +0900

    Add files via upload

---
+++ PE1/Migrations/202105130203584_DivisionId.cs
@@ -0,0 +1,48 @@
+﻿namespace PE1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DivisionId : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Employee", "Name", "dbo.Division");
+            DropForeignKey("dbo.Bug", "Division_Name", "dbo.Division");
+            DropIndex("dbo.Bug", new[] { "Division_Name" });
+            DropIndex("dbo.Employee", new[] { "Name" });
+            RenameColumn(table: "dbo.Bug", name: "Division_Name", newName: "Division_Id");
+            DropPrimaryKey("dbo.Division");
+            AddColumn("dbo.Employee", "Division_Id", c => c.Int());
+            AddColumn("dbo.Division", "Id", c => c.Int(nullable: false, identity: true));
+            AlterColumn("dbo.Bug", "Division_Id", c => c.Int());
+            AlterColumn("dbo.Employee", "Name", c => c.String());
+            AlterColumn("dbo.Division", "Name", c => c.String());
+            AddPrimaryKey("dbo.Division", "Id");
+            CreateIndex("dbo.Bug", "Division_Id");
+            CreateIndex("dbo.Employee", "Division_Id");
+            AddForeignKey("dbo.Employee", "Division_Id", "dbo.Division", "Id");
+            AddForeignKey("dbo.Bug", "Division_Id", "dbo.Division", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Bug", "Division_Id", "dbo.Division");
+            DropForeignKey("dbo.Employee", "Division_Id", "dbo.Division");
+            DropIndex("dbo.Employee", new[] { "Division_Id" });
+            DropIndex("dbo.Bug", new[] { "Division_Id" });
+            DropPrimaryKey("dbo.Division");
+            AlterColumn("dbo.Division", "Name", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.Employee", "Name", c => c.String(maxLength: 128));
+            AlterColumn("dbo.Bug", "Division_Id", c => c.String(maxLength: 128));
+            DropColumn("dbo.Division", "Id");
+            DropColumn("dbo.Employee", "Division_Id");
+            AddPrimaryKey("dbo.Division", "Name");
+            RenameColumn(table: "dbo.Bug", name: "Division_Id", newName: "Division_Name");
+            CreateIndex("dbo.Employee", "Name");
+            CreateIndex("dbo.Bug", "Division_Name");
+            AddForeignKey("dbo.Bug", "Division_Name", "dbo.Division", "Name");
+            AddForeignKey("dbo.Employee", "Name", "dbo.Division", "Name");
+        }
+    }
+}
---
+++ PE1/Migrations/202105130208276_EmployeeDivisionLink.cs
@@ -0,0 +1,20 @@
+﻿namespace PE1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeDivisionLink : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Employee", name: "Division_Id", newName: "DivisionId");
+            RenameIndex(table: "dbo.Employee", name: "IX_Division_Id", newName: "IX_DivisionId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Employee", name: "IX_DivisionId", newName: "IX_Division_Id");
+            RenameColumn(table: "dbo.Employee", name: "DivisionId", newName: "Division_Id");
+        }
+    }
+}
